24462c89f52b7bb906b3188737aa2437f3d8c478
Tinamehta0911 <tiinarjun123@gmail.com>
2026-02-25 10:35:30 +0530

    first initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        questions : "which device is required for internet connection ?",
+        option1: "modem",
+        option2: "router",
+        option3: "lan cable",
+        option4: "pen drive",
+        answer: 1,
+    },
+    {
+        questions : "Which continent has the highest number of countries ?",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North America",
+        option4: "Africa",
+        answer: 4,
+    },
+    {
+        questions : "Junk email is also called ?",
+        option1: "Spam",
+        option2: "Fake",
+        option3: "Archiver",
+        option4: "Bin",
+        answer: 1,
+    },
+    {
+        questions : "A computer cannot Boot if it is not have the ?",
+        option1: "Application Software",
+        option2: "Internet",
+        option3: "Operating System",
+        option4: "Mouse",
+        answer: 3,
+    },
+    {
+        questions : "First page of website is termed as ?",
+        option1: "Index page",
+        option2: "HomePage",
+        option3: "Sitemap",
+        option4: "Pen Drive",
+        answer: 2,
+    }
+];
